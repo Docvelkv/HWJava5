cca81cc8aee4b31fe3d6692aefb674d34bdcc582
Docvel <docvel@yandex.ru>
2023-04-13 08:00:04 +0300

    task is solved

--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -1,7 +1,42 @@
+import java.util.Deque;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 public class Methods {
 
-    public static boolean isCorrectParentheses(String inputStr) {
-
-        return true;
+    /**
+     * Метод проверяет корректность расстановки скобок в строке
+     * (задача мозг взорвала)
+     * @param inpStr входящая строка
+     * @return да или нет
+     */
+    public static boolean isCorrectParentheses(String inpStr) {
+        // достаём все скобки из входящей строки в порядке их расположения
+        inpStr = inpStr.replaceAll("[^\\[\\](){}<>]+", "");
+        // проверяем длину строки на чётность
+        if (inpStr.length() % 2 != 0) return false;
+        else {
+            // создаём словарь, где ключом будет закрывающая скобка,
+            // а значением открывающая
+            Map<Character, Character> map = new LinkedHashMap<>();
+            map.put(')', '(');
+            map.put(']', '[');
+            map.put('}', '{');
+            map.put('>', '<');
+            // создаём очередь, в которую будем добавлять только значения из словаря
+            // если символ строки совпал с этим значением, а удалять в порядке очерёдности
+            // если есть совпадение символа с ключом и проверять, что удалённая скобка
+            // являлась значением словаря (при несовпадении возвращаем false)
+            Deque<Character> deq = new LinkedList<>();
+            for (char p: inpStr.toCharArray()) {
+                if (map.containsValue(p)) deq.addFirst(p);
+                else if (map.containsKey(p)) {
+                    if (deq.isEmpty() || deq.removeFirst() != map.get(p)) return false;
+                }
+            }
+            // если очередь пустая возвращаем true, нет - false
+            return deq.isEmpty();
+        }
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -4,6 +4,24 @@
  */
 public class Task {
     public static void main(String[] args) {
+        String str = "String [^(<){InpStr = InpStr.}\\]\\[replaceAll(\"]+\",>\"\");"; // false
+        String str1 = "(InpStr)"; // true
+        String str2 = "repl(ace"; // false
+        String str3 = "(String))"; // false
+        String str4 = "(args((String)))"; // true
+        String str5 = "()[InpStr\\]{}<>"; // true
+        String str6 = "([InpStr.}\\)]"; // false
+        String str7 = "]false[true]"; // false
+        String str8 = "[]{<()[]<>>}"; // true
 
+        System.out.println(Methods.isCorrectParentheses(str));
+        System.out.println(Methods.isCorrectParentheses(str1));
+        System.out.println(Methods.isCorrectParentheses(str2));
+        System.out.println(Methods.isCorrectParentheses(str3));
+        System.out.println(Methods.isCorrectParentheses(str4));
+        System.out.println(Methods.isCorrectParentheses(str5));
+        System.out.println(Methods.isCorrectParentheses(str6));
+        System.out.println(Methods.isCorrectParentheses(str7));
+        System.out.println(Methods.isCorrectParentheses(str8));
     }
 }
